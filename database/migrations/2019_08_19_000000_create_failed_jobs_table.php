4d622bfe3e5fcc721c95b4d7d1ded9590d89ef95
Pushpak Chhajed <pushpak1300@gmail.com>
2024-11-27 09:14:23 +0000

    Changes

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -14,8 +14,6 @@ class CreateFailedJobsTable extends Migration
     public function up()
     {
         Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            
             $table->id();
             $table->string('uuid')->unique();
             $table->text('connection');
